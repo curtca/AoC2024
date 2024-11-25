51ed9e8aeafbde61ff185e5589abfa8b4991f387
Curt Carpenter <curt.carpenter@live.com>
2024-11-25 14:52:36 -0700

    template for daily projects

---
+++ template/lib/lib.cs
@@ -0,0 +1,10 @@
+﻿using System.Net;
+
+namespace lib;
+
+public class Lib
+{
+    public static long Function(string input) {
+        return 0;
+    }
+}
---
+++ template/test/test.cs
@@ -0,0 +1,26 @@
+﻿namespace test;
+
+public class Test
+{
+    [Theory]
+    [MemberData(nameof(Data))]
+    public void Test1(string value, long number)
+    {
+        Assert.Equal(number, lib.Lib.Function(value));
+    }
+
+    public static IEnumerable<object[]> Data =>
+        new List<object[]>
+        {
+            new object[] { sample, 0 },
+            new object[] { input, 0 },
+        };    
+
+static string sample = 
+@"";
+
+static string input = 
+@"";
+
+
+}
