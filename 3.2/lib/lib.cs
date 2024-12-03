472be3dcdc3b19a7872007418230f4a082f19b37
Curt Carpenter <curt.carpenter@live.com>
2024-12-03 11:20:49 -0800

    3.2

--- 3.2/lib/lib.cs
+++ 3.2/lib/lib.cs
@@ -10,10 +10,23 @@ public class Lib
 {
     public static long Function(string input)
     {
-        Regex re = new Regex("(?:mul\\((\\d+),(\\d+)\\))");
-        long sum = re.Matches(input).Sum(match => {
-            return int.Parse(match.Groups[1].Value) * int.Parse(match.Groups[2].Value);
-        });
+        Regex re = new Regex(@"(?:don't\(\)|do\(\)|mul\((\d+),(\d+)\))");
+        long sum = 0;
+        bool enabled = true;
+        foreach (Match match in re.Matches(input)) {
+            switch (match.Value) {
+                case "do()":
+                    enabled = true;
+                    break;
+                case "don't()":
+                    enabled = false;
+                    break;
+                default:
+                    if (enabled)
+                        sum += int.Parse(match.Groups[1].Value) * int.Parse(match.Groups[2].Value);
+                    break;
+            }
+        };
         return sum;
     }
 }
