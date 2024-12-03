efc176b80f42d7fe639df9c3897f7b458146bf46
Curt Carpenter <curt.carpenter@live.com>
2024-12-03 10:27:14 -0800

    3.1

---
+++ 3.1/lib/lib.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text.RegularExpressions;
+
+namespace lib;
+
+public class Lib
+{
+    public static long Function(string input)
+    {
+        Regex re = new Regex("(?:mul\\((\\d+),(\\d+)\\))");
+        long sum = re.Matches(input).Sum(match => {
+            return int.Parse(match.Groups[1].Value) * int.Parse(match.Groups[2].Value);
+        });
+        return sum;
+    }
+}
