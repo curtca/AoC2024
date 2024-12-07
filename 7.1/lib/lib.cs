c958c6e3317c77a46d755702edddd02f32a8bef1
Curt Carpenter <curt.carpenter@live.com>
2024-12-07 13:38:51 -0800

    7.1

---
+++ 7.1/lib/lib.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using Microsoft.VisualBasic;
+
+public class Lib
+{
+    public static long Function(string value)
+    {
+        var eqs = value.Split(Environment.NewLine).Select(line => {
+            var parts = line.Split(":");
+            return new EQ(long.Parse(parts[0]), parts[1].Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(n => long.Parse(n)).ToList());
+        });
+
+        return eqs.Where(Solveable).Sum(eq => eq.result);   
+    }
+
+    private static bool Solveable(EQ eq)
+    {
+        long first = eq.operands.First();
+        var rest = eq.operands.Skip(1).ToList();
+
+        return SolveHelper(eq.result, first, Ops.Plus, rest)
+            || SolveHelper(eq.result, first, Ops.Times, rest);
+    }
+
+    private static bool SolveHelper(long result, long sofar, Ops op, List<long> operands)
+    {
+        if (operands.Count == 0)
+            return result == sofar;
+
+        long next = operands.First();
+
+        switch (op) {
+        case Ops.Plus:
+            sofar += next;
+            break;
+        case Ops.Times:
+            sofar *= next;
+            break;
+        default:
+            break;
+        }
+
+        var rest = operands.Skip(1).ToList();
+        return SolveHelper(result, sofar, Ops.Plus, rest) 
+            || SolveHelper(result, sofar, Ops.Times, rest);
+    }
+}
+
+public enum Ops
+{
+    Plus, Times
+}
+
+public struct EQ
+{
+    public long result;
+    public List<long> operands;
+
+    public EQ(long result, List<long> operands)
+    {
+        this.result = result;
+        this.operands = operands;
+    }
+}
+
