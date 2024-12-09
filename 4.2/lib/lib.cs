8649c853b19b4bd7c35269dbd2e53017f93082c8
Curt Carpenter <curt.carpenter@live.com>
2024-12-09 11:19:15 -0800

    refactor 4.2

--- 4.2/lib/lib.cs
+++ 4.2/lib/lib.cs
@@ -11,19 +11,20 @@ public class Lib
 
     public static long Function(string value)
     {
-        var lines = value.Split("\r\n").ToArray();
+        var lines = value.Split(Environment.NewLine).ToArray();
 
         return Enumerable.Range(1, lines.Length - 2)
-            .SelectMany(r => Enumerable.Range(1, lines[0].Length - 2).Select(c => (Row: r, Col: c)))
-            .Count(start => IsAinMAS(lines, start.Row, start.Col));
-    }
+            .SelectMany(r => Enumerable.Range(1, lines[0].Length - 2).Select(c => (row: r, col: c)))
+            .Count(start => {
+                if (lines[start.row][start.col] != 'A') return false;
 
-    private static bool IsAinMAS(string[] lines, int row, int col)
-    {
-        if (lines[row][col] != 'A') return false;
-
-        char[] c = new char[] {lines[row-1][col-1], lines[row-1][col+1], lines[row+1][col+1], lines[row+1][col-1]};
-        return valid.Any(v => v.SequenceEqual(c));
+                char[] c = new char[] {
+                    lines[start.row-1][start.col-1], 
+                    lines[start.row-1][start.col+1], 
+                    lines[start.row+1][start.col+1], 
+                    lines[start.row+1][start.col-1]};
+                return valid.Any(v => v.SequenceEqual(c));
+            });
     }
 }    
 
