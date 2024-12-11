a65d714c0a3bbf14c909e5e87994c0a6b2b646f8
Curt Carpenter <curt.carpenter@live.com>
2024-12-11 07:47:34 -0800

    11.1

---
+++ 11.1/lib/lib.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class Lib
+{
+    public static long Function(string value)
+    {
+        var stones = value.Split(' ').Select(long.Parse).ToList();
+        for (int b = 0; b < 25; b++) {
+            for (int i = 0; i < stones.Count; i++) {
+                string str = stones[i].ToString();
+                if (stones[i] == 0) 
+                    stones[i] = 1;
+                else if (str.Length % 2 == 0) {
+                    stones[i] = long.Parse(str.Substring(str.Length / 2, str.Length / 2));
+                    stones.Insert(i, long.Parse(str.Substring(0, str.Length / 2)));
+                    i++;
+                }
+                else
+                    stones[i] *= 2024;
+            }
+        }  
+        return stones.Count;    }
+}
---
+++ 11.1/test/test.cs
@@ -0,0 +1,23 @@
+﻿public class Test
+{
+    [Theory]
+    [MemberData(nameof(Data))]
+    public void Test1(string value, long number)
+    {
+        Assert.Equal(number, Lib.Function(value));
+    }
+
+    public static IEnumerable<object[]> Data =>
+        new List<object[]>
+        {
+            new object[] { sample, 55312 },
+            new object[] { input, 231278 },
+        };    
+
+static string sample = 
+@"125 17";
+
+static string input = 
+@"1750884 193 866395 7 1158 31 35216 0";
+
+}
