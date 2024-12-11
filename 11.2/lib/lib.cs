60fc0f25104a3afdc3c68767eaac6c79b65ea892
Curt Carpenter <curt.carpenter@live.com>
2024-12-11 14:58:00 -0800

    11.2

---
+++ 11.2/lib/lib.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class Lib
+{
+    private static Dictionary<long, long> stones = null!;
+    public static long Function(string input, int blinks)
+    {
+        stones = new Dictionary<long, long>(); // key: stone number, value: quantity
+        
+        foreach (var stonenum in input.Split(' ').Select(long.Parse)) 
+            stones.Add(stonenum, 1);
+
+        for (int b = 0; b < blinks; b++) {
+            var copy = stones; // going to need to add/remove elements while blinking
+            stones = new Dictionary<long, long>();
+            foreach (var stone in copy) {
+                long num = stone.Key, count = stone.Value;
+                string str = num.ToString();
+                if (num == 0) 
+                    AddStones(1, count);
+                else if (str.Length % 2 == 0) {
+                    AddStones(long.Parse(str.Substring(0, str.Length / 2)), count);
+                    AddStones(long.Parse(str.Substring(str.Length / 2, str.Length / 2)), count);
+                }
+                else
+                    AddStones(num * 2024, count);
+            }
+        }  
+        return stones.Sum(s => s.Value);    
+    }
+
+    static void AddStones(long value, long count)
+    {
+        stones[value] = stones.GetValueOrDefault(value, 0) + count;
+    }
+}
---
+++ 11.2/test/test.cs
@@ -0,0 +1,24 @@
+﻿public class Test
+{
+    [Theory]
+    [MemberData(nameof(Data))]
+    public void Test1(string value, int blinks, long number)
+    {
+        Assert.Equal(number, Lib.Function(value, blinks));
+    }
+
+    public static IEnumerable<object[]> Data =>
+        new List<object[]>
+        {
+            new object[] { sample, 25, 55312 },
+            new object[] { input, 25, 231278 },
+            new object[] { input, 75, 274229228071551 },
+        };    
+
+static string sample = 
+@"125 17";
+
+static string input = 
+@"1750884 193 866395 7 1158 31 35216 0";
+
+}
