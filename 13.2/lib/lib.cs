35f9c3bc87d8548df2050ef91fb18a70ec392a65
Curt Carpenter <curt.carpenter@live.com>
2024-12-14 11:21:27 -0800

    13.2 extraneous usings

--- 13.2/lib/lib.cs
+++ 13.2/lib/lib.cs
@@ -1,7 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
-using System.Runtime.Intrinsics.X86;
 using System.Text.RegularExpressions;
 
 public class Lib
