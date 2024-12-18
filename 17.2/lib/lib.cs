82fe739d270125a1f53683bbb1b007fd816febc2
Curt Carpenter <curt.carpenter@live.com>
2024-12-18 07:47:27 -0800

    17.2 checkpoint

---
+++ 17.2/lib/lib.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+public class Lib
+{
+    public static string Function(string input)
+    {
+        var lines = input.Split("\r\n").ToList();
+        int A = int.Parse(Regex.Match(lines[0], @"\d+").Value);
+        int B = int.Parse(Regex.Match(lines[1], @"\d+").Value);
+        int C = int.Parse(Regex.Match(lines[2], @"\d+").Value);
+        var program = Regex.Matches(lines[4], @"\d+").Select(m => int.Parse(m.Value)).ToList();
+
+        int ip = 0;
+        var output = new List<int>();
+
+        while (ip < program.Count) {
+            int lop = program[ip + 1];
+            int cop = lop;
+            if      (cop == 4) cop = A;
+            else if (cop == 5) cop = B;
+            else if (cop == 6) cop = C;
+
+            switch(program[ip]) {
+                case 0: // adv
+                    A = A / (1 << cop);
+                    break;
+                case 1: // bxl
+                    B ^= lop;
+                    break;
+                case 2: // bst
+                    B = cop % 8;
+                    break;
+                case 3: // jnz
+                    if (A != 0) {
+                        ip = lop;
+                        ip -= 2;
+                    }
+                    break;
+                case 4: // bxc
+                    B ^= C;
+                    break;
+                case 5: // out
+                    output.Add(cop % 8);
+                    break;
+                case 6: // bdv
+                    B = A / (1 << cop);
+                    break;
+                case 7: // cdv
+                    C = A / (1 << cop);
+                    break;
+
+            }
+
+            ip += 2;
+        }
+
+        return string.Join(",", output);
+    }
+}
+
+/* 2,4,1,3,7,5,1,5,0,3,4,1,5,5,3,0
+
+ 0: 2,4 B = A % 8
+ 2: 1,3 B = B ^ 3
+ 4: 7,5 C = A / (1 << B)
+ 6: 1,5 B = B ^ 5
+ 8: 0,3 A = A / 8
+10: 4,1 B = B ^ C
+12: 5,5 Output B % 8
+14: 3,0 If A!=0 GOTO 0
+
+
+
+
+*/
---
+++ 17.2/test/test.cs
@@ -0,0 +1,32 @@
+﻿public class Test
+{
+    [Theory]
+    [MemberData(nameof(Data))]
+    public void Test1(string value, string output)
+    {
+        Assert.Equal(output, Lib.Function(value));
+    }
+
+    public static IEnumerable<object[]> Data =>
+        new List<object[]>
+        {
+            new object[] { sample, "4,6,3,5,6,3,5,2,1,0" },
+            new object[] { input, "6,7,5,2,1,3,5,1,7" },
+        };    
+
+static string sample = 
+@"Register A: 729
+Register B: 0
+Register C: 0
+
+Program: 0,1,5,4,3,0";
+
+static string input = 
+@"Register A: 21539243
+Register B: 0
+Register C: 0
+
+Program: 2,4,1,3,7,5,1,5,0,3,4,1,5,5,3,0";
+
+
+}
